575e818c76d2d194af6e586e6e367cf91b33cd37
mneway1973 <mneway73@gmail.com>
2025-01-19 01:06:00 -0500

    Added CORS headers

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -14,6 +14,7 @@ from service.models import db, Account, init_db
 from service.routes import app
 from service import talisman
 
+
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/postgres"
 )
@@ -193,3 +194,10 @@ class TestAccountService(TestCase):
         """It should not Read an Account that is not found"""
         resp = self.client.put(f"{BASE_URL}/0")
         self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_cors_security(self):
+        """It should return a CORS header"""
+        response = self.client.get('/', environ_overrides=HTTPS_ENVIRON)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        # Check for the CORS header
+        self.assertEqual(response.headers.get('Access-Control-Allow-Origin'), '*')
